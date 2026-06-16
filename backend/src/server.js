0f8338081a8dedc048d70d15a9d1eefbbcf5bf6d
Sourav T.P <tpsourav2@gmail.com>
2026-06-16 23:26:05 +0530

    Added  project files

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,8 @@ const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Connect to Database (Handle gracefully if Mongo is offline)
 connectDB();
