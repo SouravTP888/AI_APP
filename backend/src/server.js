4acd4a03ef1e459823ec5cc6018a538182caaac1
Sourav T.P <tpsourav2@gmail.com>
2026-06-17 00:43:34 +0530

    fix cors for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,13 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://ai-app-beta-six.vercel.app/"
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
